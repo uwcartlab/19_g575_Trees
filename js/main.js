e9892ceae5026f67260b675a55761d4dd3c6024d
meganroessler <mroessler@wisc.edu>
2019-04-22 13:17:55 -0500

    Add basemap
    Added basemep in main.js, map div in index.html, and map div styles in style.css

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 /* Geography 575 Final Project
 April 2019 */
+//Create basemap, import OSM tiles
+function createMap(){
+	//Create map
+	var map = L.map('map', {
+		center: [38, -97],
+		zoom: 3.5
+	});
+	//Add tiles
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+	
+	//***CALL DATA***
+};
+	
+//Call function to create map
+$(document).ready(createMap);
